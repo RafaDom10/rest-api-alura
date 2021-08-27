74cb0d728e745beef734100d706b79c67ee21680
RafaDom10 <r-domingues@outlook.com>
2021-08-27 14:46:20 -0300

    initial commit

---
+++ src/controllers/atendimentos.js
@@ -0,0 +1,10 @@
+module.exports = (app) => {
+  app.get('/atendimentos', (request, response) => {
+    response.send('você está na rota de atendimentos e está realizando um GET');
+  });
+
+  app.post('/atendimentos', (request, response) => {
+    console.log(request.body);
+    response.send('Você está na rota de atendimentos e está realizando um POST');
+  });
+};
